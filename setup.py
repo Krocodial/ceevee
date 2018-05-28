aea50d4193adab1ded19ee1333c9a4d8f7168260
krocodial <xxkraaklxx@gmail.com>
2018-05-28 13:23:32 -0700

    legal

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
 	version="1.0.0",
 	author="Louis Kraak",
 	author_email="xxkraaklxx@gmail.com",
-	description="For when you're feeling vulnerable",
+	description="For when you're feeling vulnerable, GPL-3.0-or-later",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/Krocodial/ceevee",
